a5ebfac71c711b4edd989637b535a37acda769c9
monke-boi-hackerman <91062755+monke-hackerman@users.noreply.github.com>
2023-02-09 12:16:56 +0100

    i gto sessions to work now i gtg make some sceahers and ythat stuff

--- appa.js
+++ appa.js
@@ -6,7 +6,10 @@ const db = require("better-sqlite3")("database.db");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "wwwecyn"))); //world wide web except china y northkorea
+
+const rootpath = path.join(__dirname, "wwwecyn")
+
+//app.use(express.static(path.join(__dirname, "wwwecyn"))); //world wide web except china y northkorea
 app.use(express.urlencoded({ extended: true }))
 
 app.use(session({
@@ -14,6 +17,12 @@ app.use(session({
     resave: false,
     saveUninitialized: false
 }))
+app.get("/", (req, res) =>{
+    res.redirect("/index.html")
+})
+app.get("/reg.html", (req, res) =>{
+    res.sendFile(rootpath + "/reg.html")
+})
 
 app.get("/visits", (req, res) =>{
     if(req.session.visitsCount == undefined){
@@ -33,7 +42,7 @@ app.post(("/NyBruk"), async (req, res) => {
 
     db.prepare("INSERT INTO user (name, email, hash) VALUES (?, ?, ?)").run(svr.navn, svr.email, hash)
 
-    res.redirect("/logg.html")
+    res.redirect("/index.html")
 })
 app.post(("/login"), async (req, res) => {
     let svr = req.body
@@ -41,32 +50,32 @@ app.post(("/login"), async (req, res) => {
     let userData = db.prepare("SELECT * FROM user WHERE email = ?").get(svr.email);
     
     if(await bcrypt.compare(svr.password, userData.hash)) {
-        console.log("ye got inn")
-        res.redirect("/index.html")
+        console.log("loggedinn")
         req.session.loggedin = true
-        if(req.session.loggedin === true){
-            console.log("session started")
-        }
-    } else {
+        console.log(req.session.loggedin)
+        res.redirect("/index.html")
+        
+    }else {
         console.log("fuck off")
-        res.redirect("back")
+        res.sendFile(rootpath + "/logg.html")
         req.session.loggedin = false
     }
 })
 app.post(("/signout"), async (req, res) => {
     console.log("byebye")
-    res.redirect("/logg.html")
+    res.sendFile(rootpath + "/logg.html")
     req.session.loggedin = false
     if(req.session.loggedin === false){
         console.log("session ended")
     }
 })
 app.get("/index.html", (req, res) =>{
-    if(req.session.loggedin !== true){
-        res.redirect("/logg.html")
-        console.log("not logged inn")
-    }else{
+    if(req.session.loggedin){
         console.log("ye got inn")
+        res.sendFile(rootpath + "/index.html")
+    }else{
+        res.sendFile(rootpath + "/logg.html")
+        console.log("not logged inn")
     }
 })
 app.listen("3000", () => {
